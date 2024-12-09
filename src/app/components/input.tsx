f4e2bf7ab66da92196ab3e9ff9e0b9eb5bf3ce6d
Maxim <trubitsynmaxaug@gmail.com>
2024-12-09 19:37:39 +0300

    Create navigation buttons
    * Created 2 buttons: one for creating new categories, the second
    for viewing all existing categories

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,11 +1,16 @@
 
+
 export default function Search() {
 	return (
 		<div className="search">
-			<div className="max-w-[1200px] mx-auto h-full flex items-center justify-center">
+			<div className="max-w-[1200px] mx-auto h-full flex flex-col items-center justify-center">
 			<form className="w-full">
-				<input type="text" className='border-[1px] border-solid w-full p-2 text-2xl' placeholder='Напишите название галереи'/>
+				<input type="text" className='border-[1px] border-solid border-stone-950 w-full p-2 text-2xl rounded-lg mb-4' placeholder='Напишите название галереи...'/>
 			</form>
+			<div className='flex justify-between w-full gap-12'>
+				<a href="#" className='border w-2/4 text-center py-4 text-xl rounded-lg mb-4 bg-[#f5f5dc]'>Создать категорию</a>
+				<a href="#" className='border w-2/4 text-center py-4 text-xl rounded-lg mb-4 bg-[#f5f5dc]'>Посмотреть существующие</a>
+			</div>
 			</div>
 		</div>
 	);
